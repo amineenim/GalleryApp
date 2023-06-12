195db6f576fa8ba058c7744505d6e6b5fafcc0f7
amineenim <aminemaourid1995@gmail.com>
2023-06-12 18:29:47 +0200

    adding the possibility to clear messages with an x mark click

--- likes/urls.py
+++ likes/urls.py
@@ -5,4 +5,6 @@ app_name = 'likes'
 urlpatterns = [
     path('<int:photo_id>/likes/', views.likes_per_photo, name="likes_per_photo"),
     path('<int:photo_id>/add_like/', views.add_like, name='add_like'),
+    # this route is for clearing messages if there are any 
+    path('messages/clear/', views.clear_messages, name='clear_messages'),
 ]
--- likes/views.py
+++ likes/views.py
@@ -2,19 +2,26 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 from photoshare.models import Photo
 from .models import Like
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 # Create your views here.
 
 # view that handles diplaying likes for a given Photo 
+@login_required
 def likes_per_photo(request, photo_id) :
     # get the photo
     photo = get_object_or_404(Photo, pk=photo_id)
-    # get the likes for the photo 
-    likes_for_photo = Like.objects.filter(photo=photo)
-    return render(request, 'likes/likes_per_photo.html', {'likes' : likes_for_photo, 'photo' : photo})
-
-
+    # check if the photo corresponds to the current user 
+    if photo.created_by == request.user :
+        # get the likes for the photo 
+        likes_for_photo = Like.objects.filter(photo=photo)
+        return render(request, 'likes/likes_per_photo.html', {'likes' : likes_for_photo, 'photo' : photo})
+    else : 
+        messages.error(request, "unauthorized to visit this page")
+        return redirect(reverse('gallery'))
 
 # view that handles the form to add a new like to a photo 
+@login_required
 def add_like(request, photo_id) :
     photo = get_object_or_404(Photo, pk=photo_id)
     #return render(request, 'likes/add_like.html', {'photo' : photo})
@@ -34,3 +41,10 @@ def add_like(request, photo_id) :
             photo.save()
             return redirect(reverse('detail_photo', args=(photo.id,)))
         
+# view that handles clearing nessages
+def clear_messages(request) :
+    if request.method == "POST" :
+        redirect_url = request.POST.get('previous_url')
+        storage = messages.get_messages(request)
+        storage.used = True
+        return redirect(redirect_url)
