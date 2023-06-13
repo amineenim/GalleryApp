6bdc1ee70ed645ba730b0225c05a57ad45cecf22
amineenim <aminemaourid1995@gmail.com>
2023-06-13 17:09:22 +0200

    adding the functionnality to display published since when on the comment model

--- likes/admin.py
+++ likes/admin.py
@@ -9,7 +9,7 @@ class LikeAdmin(admin.ModelAdmin):
 admin.site.register(Like, LikeAdmin)
 
 class CommentAdmin(admin.ModelAdmin) :
-    list_display = ['comment_text','photo', 'created_by', 'created_at']
+    list_display = ['comment_text','photo', 'created_by', 'created_at', 'get_when_created']
     list_filter = ['created_by', 'created_at', 'photo']
 
 admin.site.register(Comment, CommentAdmin)
--- likes/models.py
+++ likes/models.py
@@ -1,6 +1,9 @@
+import datetime
 from django.db import models
 from photoshare.models import Photo
 from django.contrib.auth.models import User
+from django.utils import timezone
+from django.contrib import admin
 # Create your models here.
 
 class Like(models.Model) :
@@ -25,3 +28,30 @@ class Comment(models.Model) :
     def __str__(self) :
         return self.comment_text
     
+    @admin.display(
+            boolean=False,
+            ordering="-created_at",
+            description= "published since when"
+    )
+    def get_when_created(self) :
+        time_now = timezone.now()
+        if time_now - datetime.timedelta(days=1) <= self.created_at <= time_now :
+            if time_now - datetime.timedelta(hours=1) <= self.created_at :
+                difference = time_now - self.created_at
+                diffence_in_seconds = difference.total_seconds()
+                difference_in_minutes = int(diffence_in_seconds//60)
+                if difference_in_minutes == 0 :
+                    seconds = int(diffence_in_seconds % 60)
+                    return f"{seconds} seconds ago"
+                return f"{difference_in_minutes} minutes ago"
+            else :
+                difference = time_now - self.created_at
+                diffence_in_seconds = difference.total_seconds()
+                hours = int(diffence_in_seconds//3600)
+                if hours == 1 :
+                    return f"1 hour ago"
+                return f"{hours} hours ago"
+        elif time_now - datetime.timedelta(days=2) <= self.created_at <= time_now - datetime.timedelta(days=1) :
+            return "yesterday"
+        else :
+            return self.created_at 
