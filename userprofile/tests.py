5bd1d4ac710dbacf167d75be15cb338256c08f1a
amineenim <aminemaourid1995@gmail.com>
2023-06-20 21:39:24 +0200

    starting tests for the get_my_profile view with the case a user is unauthenticated and trying to reach the url

--- userprofile/tests.py
+++ userprofile/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
-
+from django.urls import reverse
+from . import urls
 # Create your tests here.
+
+# class to test the operation of get_my_profile View 
+class GetMyProfileViewTests(TestCase) :
+    # tests with an unauthenticated user 
+    def test_get_my_profile_with_unauthenticated_user(self) :
+        target_url = reverse('profile:my_profile')
+        response = self.client.get(target_url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, f"{reverse('login')}?next={target_url}")
