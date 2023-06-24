a651091b4a47800a87ac7ed226619ad9cdb7a600
amineenim <aminemaourid1995@gmail.com>
2023-06-24 20:14:06 +0200

    creating a FriendshipRequest Model and registering it to the admin dashboard with customized options for diplay and filter

--- friends/admin.py
+++ friends/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import FriendshipRequest
 # Register your models here.
+
+class FriendshipRequestAdmin(admin.ModelAdmin) :
+    list_display = ['initiated_by', 'sent_to', 'created_at', 'status']
+    list_filter = ['initiated_by', 'sent_to', 'created_at', 'status']
+
+admin.site.register(FriendshipRequest, FriendshipRequestAdmin)
+
+
--- friends/models.py
+++ friends/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+# class that represents a FriendshipRequest Object 
+class FriendshipRequest(models.Model) :
+    initiated_by = models.ForeignKey(User, name='my_sent_requests', on_delete=models.CASCADE, null=False, blank=False)
+    sent_to = models.ForeignKey(User, name='my_received_requests', on_delete=models.CASCADE, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField(null=False, blank=False, default=False)
+
+
+    def __str__(self) :
+        return self.initiated_by.username + ' sent a request to ' + self.sent_to.username 
+    
