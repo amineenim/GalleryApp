b5102d60dd0a3ce11718d391a5b2566288ff05d5
amineenim <aminemaourid1995@gmail.com>
2023-06-24 23:31:56 +0200

    adding the test case for send_friendship_request view where an authenticated user sends a request to another user to whom a friendship request has not already been sent

--- friends/tests.py
+++ friends/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.contrib import messages
+from .models import FriendshipRequest
 # Create your tests here.
 
 # class to test the operation of the send_friendship_request View 
@@ -13,7 +15,7 @@ class SendFriendshipRequestViewTests(TestCase) :
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, f"{reverse('login')}?next={target_url}")
 
-    # test send_friendship_request to a user which does not exist 
+    # test send_friendship_request to a user who does not exist 
     def test_send_friendship_request_to_unexisting_user(self) :
         # create a user and autneticate him 
         User.objects.create_user(username='amine', password='test')
@@ -23,4 +25,42 @@ class SendFriendshipRequestViewTests(TestCase) :
         response = self.client.post(target_url, {})
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('gallery'))
-        
+    
+    # test send_friendship_request to a user , with no previous friendship request already sent to him by the authenticated user
+    def test_send_friendship_request_to_user_with_no_previous_friendship_request_from_authenticated_user(self) :
+        User.objects.create_user(username='receiver', password='1234')
+        # create a user and authenticate him
+        User.objects.create_user(username='amine', password='test')
+        self.client.login(username='amine', password='test')
+        # check the "receiver" profile to see if he has "add as a friend"
+        profile_url = reverse('profile:view_profile', args=('receiver',))
+        response = self.client.get(profile_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['username'], 'receiver')
+        self.assertEqual(response.context['are_we_friends'], False)
+        self.assertEqual(response.context['no_invitation'], True)
+        self.assertContains(response, 'Add as a Friend')
+        # pass 'receiver' as the username param value
+        target_url = reverse('friends:send_request', args=('receiver',))
+        response = self.client.post(target_url, {})
+        # check response status
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('profile:view_profile', args=('receiver',)))
+        # check messages 
+        my_messages = list(messages.get_messages(response.wsgi_request))
+        for message in my_messages :
+            self.assertEqual(message.tags, 'success')
+            self.assertEqual(message.message, 'Friendship request sent successefully to receiver')
+        # check that  FriendshipRequest object has been created 
+        self.assertTrue(FriendshipRequest.objects.exists())
+        # now after the request has been sent, verify "receiver" profile again
+        response = self.client.get(profile_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['are_we_friends'], False)
+        self.assertEqual(response.context['i_invited_him'], True)
+        self.assertEqual(response.context['no_invitation'], False)
+        self.assertContains(response, 'Cancel my Request')        
+
+
+
+
