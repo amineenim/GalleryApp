aa75c4c41f262fa5711713eedd56302543ddaaff
amineenim <aminemaourid1995@gmail.com>
2023-06-24 22:48:46 +0200

    adding the first test case for send_friendship_request with an unauthenticated user

--- friends/tests.py
+++ friends/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User
+from django.urls import reverse
 # Create your tests here.
+
+# class to test the operation of the send_friendship_request View 
+class SendFriendshipRequestViewTests(TestCase) :
+    # test send_friendship_request with unauthenticated user 
+    def test_send_friendship_request_with_unauthenticated_user(self) :
+        # i pass a username which doesn't exist but it doesn't matter the user is unauthenticated
+        target_url = reverse('friends:send_request', args=('test',))
+        response = self.client.post(target_url,{})
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, f"{reverse('login')}?next={target_url}")
+        
