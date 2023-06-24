2190816d96664cfd9139b4ebf01b6c5d2c4603f3
amineenim <aminemaourid1995@gmail.com>
2023-06-24 20:18:58 +0200

    running database migrations

--- friends/models.py
+++ friends/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 
 # class that represents a FriendshipRequest Object 
 class FriendshipRequest(models.Model) :
-    initiated_by = models.ForeignKey(User, name='my_sent_requests', on_delete=models.CASCADE, null=False, blank=False)
-    sent_to = models.ForeignKey(User, name='my_received_requests', on_delete=models.CASCADE, null=False, blank=False)
+    initiated_by = models.ForeignKey(User, related_name='my_sent_requests', on_delete=models.CASCADE, null=False, blank=False)
+    sent_to = models.ForeignKey(User, related_name='my_received_requests', on_delete=models.CASCADE, null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     status = models.BooleanField(null=False, blank=False, default=False)
 
