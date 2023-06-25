5d7ecc1818c153fb713ef1dd89b9537b00e51602
amineenim <aminemaourid1995@gmail.com>
2023-06-25 20:48:58 +0200

    adding FriendsList model which represents the friends list for a given user and running migrations

--- friends/admin.py
+++ friends/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import FriendshipRequest
+from .models import FriendshipRequest, FriendsList
 # Register your models here.
 
 class FriendshipRequestAdmin(admin.ModelAdmin) :
@@ -7,5 +7,13 @@ class FriendshipRequestAdmin(admin.ModelAdmin) :
     list_filter = ['initiated_by', 'sent_to', 'created_at', 'status']
 
 admin.site.register(FriendshipRequest, FriendshipRequestAdmin)
+
+class FriendsListAdmin(admin.ModelAdmin) :
+    fields = ['belongs_to', 'friends']
+    list_display = ['belongs_to', 'get_number_of_friends']
+    list_filter = ['belongs_to']
+
+admin.site.register(FriendsList, FriendsListAdmin)
+
 
 
--- friends/models.py
+++ friends/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib import admin
 # Create your models here.
 
 # class that represents a FriendshipRequest Object 
@@ -12,4 +13,22 @@ class FriendshipRequest(models.Model) :
 
     def __str__(self) :
         return self.initiated_by.username + ' sent a request to ' + self.sent_to.username 
+    
+
+# class that reprensts a FriendsList object 
+class FriendsList(models.Model) :
+    belongs_to = models.OneToOneField(User, on_delete=models.CASCADE)
+    friends = models.ManyToManyField(User, related_name='my_friends')
+
+    def __str__(self) :
+        return self.belongs_to.username
+    
+    # customize the apperance for the admin dashboard
+    @admin.display(
+            boolean=False,
+            ordering='-get_number_of_friends',
+            description= 'number of friends',
+    )
+    def get_number_of_friends(self) :
+        return self.friends.count()
     
