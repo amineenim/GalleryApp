4e7042c3a7915633f8dfa3f1b735f41a67763321
amineenim <aminemaourid1995@gmail.com>
2023-07-10 21:32:14 +0200

    adding test case for user providing a valid email address and getting sent an email with token to reset his password

--- photoshare/tests.py
+++ photoshare/tests.py
@@ -1,6 +1,8 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from django.contrib.auth.models import User
+from .models import PasswordResetToken
 from django.urls import reverse
+from django.core import mail
 # Create your tests here.
 # class to test the operation of reset_password view
 class PasswordResetViewTests(TestCase) :
@@ -47,10 +49,38 @@ class PasswordResetViewTests(TestCase) :
         self.assertContains(response, 'an email with a link to reset your password will be sent to your email address')
     
     # test with unauthenticated user and a valid email which doesn't correspond to no user
-    def test_reset_password_with_post_request_and_valid_email_not_referencing_no_user(self) :
+    def test_reset_password_with_post_request_and_valid_email_referencing_no_user(self) :
         target_url = reverse('reset_password')
         # valid email, not belonging to no user 
         response = self.client.post(target_url, {'email' : 'test.test@gmail.com'})
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Given email address does not correspond to a user')
         self.assertContains(response, 'an email with a link to reset your password will be sent to your email address')
+
+    # test with unauthenticated user and a valid email belonging to a user 
+    # override the email backend to temporarily use 'locmem' which allows
+    # storing the sent mails in memory instead of sending them via SMTP
+    @override_settings(EMAIL_BACKEND='django.core.mail.backends.locmem.EmailBackend')
+    def test_reset_password_with_post_request_and_valid_email_belonging_to_a_user(self) :
+        # create a user 
+        user = User.objects.create_user(username='amine', email='amine_maourid@gmail.com', password='1234')
+        target_url = reverse('reset_password')
+        response = self.client.post(target_url, {'email' : 'amine_maourid@gmail.com'})
+        # check for response status
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Password reset url sent, check your email')
+        # check that a PasswordResetToken object has been created 
+        self.assertTrue(PasswordResetToken.objects.exists())
+        self.assertTrue(PasswordResetToken.objects.filter(user=user).exists())
+        # check for mail sending 
+        self.assertEqual(len(mail.outbox), 1)
+        sent_mail = mail.outbox[0]
+        # check email attributes 
+        self.assertEqual(sent_mail.subject, 'Password Reset')
+        self.assertEqual(sent_mail.from_email, 'aminemaourid1@gmail.com')
+        self.assertEqual(sent_mail.to, ['amine_maourid@gmail.com'])
+        # check for mail content 
+        self.assertIn('to reset your password, click the following url :', sent_mail.body)
+        reset_url = f"http://localhost:8000/gallery/accounts/resetpassword/?token={PasswordResetToken.objects.first().token}"
+        self.assertIn(reset_url, sent_mail.body)
+
