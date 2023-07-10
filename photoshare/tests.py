d61dd964b9a82b506d4faa104f87dffd482fc219
amineenim <aminemaourid1995@gmail.com>
2023-07-10 20:16:51 +0200

    adding two test cases for reset_password view, the first with an authenticated user and a get request to get form for email submission, the second test case correponds to submitting an empty value for the email in the form

--- photoshare/tests.py
+++ photoshare/tests.py
@@ -18,4 +18,21 @@ class PasswordResetViewTests(TestCase) :
         response = self.client.post(target_url, {})
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('gallery'))
-        
+    
+    # test with unauthenticated user in get request 
+    def test_reset_password_with_unauthenticated_user_and_get_request(self) :
+        target_url = reverse('reset_password')
+        response = self.client.get(target_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'an email with a link to reset your password will be sent to your email adress')
+    
+    # the following tests serve for the form where the user enters his email
+    # address in order to get a reset password link 
+
+    # test with unauthenticated user and post request with empty email value submitted
+    def test_reset_password_with_post_request_and_empty_value_for_email_input(self) :
+        target_url = reverse('reset_password')
+        response = self.client.post(target_url, {'email' : ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Enter an email address, empty value submitted')
+        self.assertContains(response, 'an email with a link to reset your password will be sent to your email adress')
