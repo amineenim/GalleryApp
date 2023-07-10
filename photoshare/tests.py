f12570295463667ef6462622d21712ef505ec1b8
amineenim <aminemaourid1995@gmail.com>
2023-07-10 19:49:56 +0200

    adding the first test case for reset_password view with authenticated user

--- photoshare/tests.py
+++ photoshare/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User
+from django.urls import reverse
 # Create your tests here.
+# class to test the operation of reset_password view
+class PasswordResetViewTests(TestCase) :
+    # test with authenticated user 
+    def test_reset_password_with_authenticated_user(self) :
+        # create a user and authenticate him
+        User.objects.create_user(username='amine', password='1234')
+        self.client.login(username='amine', password='1234')
+        target_url = reverse('reset_password')
+        # test for get request
+        response = self.client.get(target_url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('gallery'))
+        # test for post request
+        response = self.client.post(target_url, {})
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('gallery'))
+        
